f9360019f634b6a907f5db15e3e5445c68ceb64d
Victoregb <67790915+psiconomicon@users.noreply.github.com>
2021-02-13 19:44:09 +0100

    Prueba2

--- Code_training/hola.py
+++ Code_training/hola.py
@@ -1,21 +1,21 @@
 #%%
-import math
-import matplotlib.pyplot as plt
+# Cargamos las librerias.
+import pandas as pd
 import numpy as np
-
-
+import matplotlib.pyplot as plt
 
 #%%
-pi = math.pi
-
-euler = math.e
-
-#Esto es otra prueba. 
-print(pi)
-print(euler)
-# %%
-
-y = np.arange(0,100)
 x = np.arange(0,100)
+y = np.arange(0,100)
+
 plt.plot(x,y)
+# %%
+#! Estoy hay que reviar.
+#? Pero que me dices.
+# TODO
+# * Esto es de otro color y es interesante verlo. 
+d_semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+
+for i in d_semana:
+    print(i)
 # %%
