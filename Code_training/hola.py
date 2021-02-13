f0239cc89f392e31b7054314e128af820319c874
Victoregb <67790915+psiconomicon@users.noreply.github.com>
2021-02-13 19:25:18 +0100

    Probandoo

---
+++ Code_training/hola.py
@@ -0,0 +1,21 @@
+#%%
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+#%%
+pi = math.pi
+
+euler = math.e
+
+#Esto es otra prueba. 
+print(pi)
+print(euler)
+# %%
+
+y = np.arange(0,100)
+x = np.arange(0,100)
+plt.plot(x,y)
+# %%
