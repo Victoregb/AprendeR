39d7c6212e0e2bfa3a3dac71f0d77bf09a05fe38
Victoregb <67790915+psiconomicon@users.noreply.github.com>
2021-02-16 14:13:02 +0100

    Primero ejecercicios

---
+++ Aprende_py/002.py
@@ -0,0 +1,24 @@
+print('Cadena vacia', str())
+print('Repr en Cadena', str(['A', 'h']))
+print('Copia en cadena', str('MJ'))
+
+##############
+
+s1 = 'Hola Don Pepito!!'
+s2 = 'Hola \"Don José\"!!'
+
+
+##############
+
+s3 = ''' Esto es una de esas cadenas que contienen 'comillas' y que son interesantes '''
+print(s3)
+
+##############
+
+print(s2)
+
+##############
+
+s4 = '\N{euro sign} \u20AC \U000020AC'
+
+print(s4)
